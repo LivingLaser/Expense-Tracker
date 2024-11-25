0196189d0f347f4c69e7bd82a06b7c4ca491dc6e
Tiyashaghosh-23 <ghoshtiyasha06@gmail.com>
2024-11-25 23:50:58 +0530

    commit

--- src/main/java/com/main/daoInterface/AdminQuery.java
+++ src/main/java/com/main/daoInterface/AdminQuery.java
@@ -4,7 +4,7 @@ public interface AdminQuery {
 	
 	String SELECT = "select * from admin where username=? and password=?";
 	String SELECT_ALL_USER = "select * from user";
-	String SELECT_ALL_RECORD = "select uid, type, name, amount, icon, DATE_FORMAT(date, '%d-%m-%Y') as date, date as old_date, time from transaction order by STR_TO_DATE(date, '%d-%m-%Y') desc";
+	String SELECT_ALL_RECORD = "select uid, type, name, amount, icon, DATE_FORMAT(date, '%d-%m-%Y') as date, date as old_date, time from transaction order by old_date desc";
 	String TOTAL_INCOME = "select ROUND(IFNULL(SUM(amount), 0), 2) as income from transaction where type='income'";
 	String TOTAL_EXPENSE = "select ROUND(IFNULL(SUM(amount), 0), 2) as expense from transaction where type='expense'";
 
